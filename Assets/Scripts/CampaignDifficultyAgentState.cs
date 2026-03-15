7ebac6912b5f91612033944eca912d2966ae7459
JotaBarbosaDev <jotambbarbosa@gmail.com>
2026-03-15 13:33:24 +0000

    Add difficulty scaling bootstrap for campaign levels

---
+++ Assets/Scripts/CampaignDifficultyAgentState.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CampaignDifficultyAgentState : MonoBehaviour
+{
+    public bool hasBaseValues;
+    public float baseMaxHealth;
+    public float baseMoveSpeed;
+    public int baseLoot;
+}
